e152311dc2bc5a8672fa5f2bd0bbb9616cfe28b1
许劲淇 <xujinqi2001@outlook.com>
2022-11-18 13:49:24 +0000

    完成List相关函数

--- List/myFunc.cpp
+++ List/myFunc.cpp
@@ -142,3 +142,174 @@ void preOdd(SeqList &L) {
     }
     L.data[low] = tmp;
 }
+
+int kth(SeqList &L, int k, int i, int j) {
+    // 使用快排思想，找到第k小的元素
+    int low = i, high = j;
+    if (low < high) {
+        int tmp = L.data[low];
+        while (low < high) {
+            while (low < high && L.data[high] >= tmp) {
+                high--;
+            }
+            L.data[low] = L.data[high];
+            while (low < high && L.data[low] >= tmp) {
+                low++;
+            }
+            L.data[high] = L.data[low];
+        }
+        if (low == k) {
+            return L.data[low];
+        } else if (low < k) {
+            return kth(L, k, low + 1, high);
+        } else {
+            return kth(L, k, i, low - 1);
+        }
+    }
+    return -1;
+}
+
+int BiSearch(SeqList L, int e) {
+    int low = 0, high = L.length - 1; // 指向当前数组的前后端
+    while (low <= high) {
+        int mid = (low + high) / 2; // 取中间位置
+        if (L.data[mid] == e) {
+            return mid; // 查找成功则返回
+        } else if (L.data[mid] > e) {
+            high = mid - 1; // 大于e则取左半
+        } else {
+            low = mid + 1; // 后半区间
+        }
+    }
+    return -1;
+}
+
+int BiSearchFirst(SeqList L, int e) {
+    // 查找第一个时，在查找过程中即使找到e，也必须判断前半没有等于e的元素
+    int low = 0, high = L.length - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (L.data[mid] <= e) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return L.data[low] == e ? low : -1;
+}
+
+int midNum(SeqList L1, SeqList L2) {
+    /**
+     * 当两个列表的中位数相等时，则该值就是中位数
+     * 当中位数不相等时，则总的中位数比如在两个中位数的区间中间
+     **/
+    int l1 = 0, l2 = 0, h1 = L1.length, h2 = L2.length, m1, m2;
+    while (l1 != h1 || l2 != h2) {
+        m1 = (l1 + h1) / 2;
+        m2 = (l2 + m2) / 2;
+        if (L1.data[m1] == L2.data[m2]) {
+            return L1.data[m1]; // 当中位数相等时，返回
+        } else if (L1.data[m1] < L2.data[m2]) {
+            // 取L1后半和L2前半，且前后舍弃的元素个数相等
+            l1 = m1 + (h1 - l1) % 2;
+            h2 = m2;
+        } else {
+            l2 = m2 + (h2 - l2) % 2;
+            h1 = m1;
+        }
+    }
+    return L1.data[l1] < L2.data[l2] ? L1.data[l1] : L2.data[l2];
+    // 最后一次比较是两个数之间的比较，更小的一个是中位数
+}
+
+int findHalf(SeqList L) {
+    /**
+     *当元素出现次数大于一半，则该元素的个数必定大于其他元素的和
+     *因此使用count记录该元素出现次数，如果最后存在count>0的元素
+     *则表明有可能存在次数大于一半的元素
+     **/
+    int count = 1, m = L.data[0];
+    for (int i = 1; i < L.length; i++) {
+        if (m == L.data[i]) {
+            count++;
+        } else {
+            count--;
+            if (m == 0) {
+                m = L.data[i];
+                count = 1;
+                // 当元素出现次数等于0时，表明该元素暂时不可能是目标元素
+                // 更新元素
+            }
+        }
+    }
+    // 如果最后存在count>0的元素，则判断该元素在线性表中出现次数是否真的大于一半
+    count = 0;
+    for (int i = 0; i < L.length; i++) {
+        if (L.data[i] == m) {
+            count++;
+        }
+    }
+    return count > (L.length / 2) ? m : -1;
+}
+
+LNode *findFirstCommon(LinkList L1, LinkList L2) {
+    /**
+     *首先计算两条链表的长度，遍历长的一天链表直到两条链表长度相等
+     *然后顺序比较，判断是否存在公共结点
+     *此处假设每条链表中不存在重复的结点
+     **/
+    int l1 = GetLenth(L1), l2 = GetLenth(L2);
+    LNode *shorter, *longer;
+    int dis;
+    if (l1 < l2) {
+        shorter = L1->next;
+        longer = L2->next;
+        dis = l2 - l1;
+    } else {
+        shorter = L2->next;
+        longer = L1->next;
+        dis = l1 - l2;
+    }
+    while (dis--) {
+        longer = longer->next;
+    } // 将长的链表指针指向两个链表长度相等的结点
+    while (longer && shorter) {
+        if (longer->data == shorter->data) {
+            return longer;
+        } // 当两个结点值相等时，该结点是公共结点
+        else {
+            longer = longer->next;
+            shorter = shorter->next;
+        }
+    }
+    return NULL;
+}
+
+int GetLenth(LinkList L) {
+    LNode *p = L;
+    int length = 0;
+    while (p->next) {
+        length++;
+        p = p->next;
+    }
+    return length;
+}
+
+bool hasLoop(LinkList L) {
+    // 使用快慢指针判断是否存在环
+    LNode *fast = L, *slow = L;
+    if (!L || !L->next || !L->next->next)
+        return false;
+    while (slow && fast && fast->next) {
+        fast = fast->next->next;
+        slow = slow->next;
+        if (equalLNode(slow, fast)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool equalLNode(LNode *q, LNode *p) {
+    return p->data == q->data;
+}
--- List/myFunc.h
+++ List/myFunc.h
@@ -11,6 +11,7 @@ typedef struct Node {
 } LNode, *LinkList;
 
 void swap(int &a, int &b);
+int GetLenth(LinkList L);
 // 删除线性表中元素为x的值
 void deleteX(SeqList &L, int x);
 void deletex(LinkList &L, int x);
@@ -36,4 +37,23 @@ void mergeLink(LinkList &L1, LinkList &L2, LinkList &L3);
 void preOdd(SeqList &L);
 
 // 查找线性表第k小元素
-void kth(SeqList L, int k, int low, int high);
+int kth(SeqList &L, int k, int i, int j);
+
+// 二分查找
+int BiSearch(SeqList L, int e);
+
+// 查找有序表第一个元素为x的下标
+int BiSearchFirst(SeqList L, int e);
+
+// 查找两个有序表的中位数
+int midNum(SeqList L1, SeqList L2);
+
+// 找出线性表中出现次数大于一半的元素
+int findHalf(SeqList L);
+
+// 找出共同后缀的两条单链表的第一个公共结点
+LNode *findFirstCommon(LinkList L1, LinkList L2);
+
+// 判断链表是否有环
+bool hasLoop(LinkList L);
+bool equalLNode(LNode *q, LNode *p);
